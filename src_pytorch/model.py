44faa35728c9e129da2e111b869e0ee5c7835886
Jaundel Simon <jaundelsimon@gmail.com>
2025-10-08 00:34:38 -0400

    Refactor MNISTCNN model architecture

--- src_pytorch/model.py
+++ src_pytorch/model.py
@@ -1,28 +1,23 @@
 import torch
-from torch import nn
-
+import torch.nn as nn
+import torch.nn.functional as F
 
 class MNISTCNN(nn.Module):
-    """Simple CNN for MNIST classification."""
-
     def __init__(self):
         super().__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Dropout(0.25),
-        )
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(64 * 14 * 14, 128),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
-            nn.Linear(128, 10),
-        )
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=5, padding=2)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=5, padding=2)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(16 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.features(x)
-        return self.classifier(x)
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
